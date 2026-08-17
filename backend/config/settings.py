cb0bda00d04f2bf5cf6846bfe00fdace61cd01a5
abhay <abhaytony17@gmail.com>
2026-08-17 20:48:18 +0530

    Configure Render allowed host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,10 +31,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv(
-    'ALLOWED_HOSTS',
-    'localhost,127.0.0.1'
-).split(',')
+ALLOWED_HOSTS = [
+    os.getenv('RENDER_EXTERNAL_HOSTNAME', 'localhost')
+]
 
 
 # Application definition
